cc5e5dd85e52afb9c30d437402c8a0939857edd4
David Jovellar <davidjovellar93@gmail.com>
2020-08-15 19:48:54 +0200

    Adapt files to Wordpress

---
+++ footer.php
@@ -0,0 +1,5 @@
+<div class="pt-1"></div>
+
+<footer class="container" style="background-color: blue;">
+    <p class="text-white text-center" style="font-size: 1.25rem;">&copy; 2020 Zona Gigantes. Todos los derechos reservados</p>
+</footer>
---
+++ header.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <!-- <link rel="stylesheet" href="css/bootstrap.css"> -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <!-- Style -->
+    <!-- <link rel="stylesheet" href="style.css"> -->
+    <link rel="stylesheet" href="<?php bloginfo('template_url');?>/style.css">
+
+    <title>Zona Gigantes</title>
+  </head>
+  <body>
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-dark container" style="background-color: blue;">
+          <a class="navbar-brand px-4" href="<?php bloginfo('template_url');?>/index.html">
+              <img src="<?php bloginfo('template_url');?>/images/other-icons/logo.png" width="50" height="50" class="img-fluid" alt="Icono Zona Gigantes" loading="lazy">
+          </a>
+          <a class="navbar-brand" href="/index.html">Zona Gigantes</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav ml-auto">
+                  <a class="nav-item nav-link mx-3" href="<?php bloginfo('template_url');?>/page.html">NOTICIAS</a>
+                  <a class="nav-item nav-link mx-3" href="#">YOUTUBE</a>
+                  <a class="nav-item nav-link mx-3" href="#">CRÓNICAS</a>
+                  <a class="nav-item nav-link mx-3" href="#">CONTACTO</a>
+              </div>
+          </div>
+      </nav>
+    </header>
