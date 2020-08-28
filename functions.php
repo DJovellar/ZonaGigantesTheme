c40017f7ce532f0860a4867dd8fc9e820151b3be
David Jovellar <davidjovellar93@gmail.com>
2020-08-28 19:19:23 +0200

    Set active class for navbar

--- functions.php
+++ functions.php
@@ -18,5 +18,13 @@ if (function_exists('add_theme_support')) {
     add_theme_support('post-thumbnails');
 }
 
+add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
+
+function special_nav_class ($classes, $item) {
+  if (in_array('current-menu-item', $classes) ){
+    $classes[] = 'active ';
+  }
+  return $classes;
+}
 
 ?>
