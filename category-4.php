7d5348c821582cb17257d3c769e847a7968cd60c
David Jovellar <davidjovellar93@gmail.com>
2020-08-29 12:26:29 +0200

    Add pagination for Categories

--- category-4.php
+++ category-4.php
@@ -5,13 +5,10 @@
     <h2>Crónicas</h2>
     <hr>
     <?php 
-        $posts = new WP_Query(array(
-        'cat' => '4',
-        ));
-        if ($posts->have_posts() ) : while ( $posts->have_posts() ) : $posts->the_post(); ?>
+        if (have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <div class="row pt-1">
             <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
-                <?php if (has_post_thumbnail()) the_post_thumbnail('post-thumbnails', array( 'class' => 'img-fluid' )) ?>
+                <?php if (has_post_thumbnail()) the_post_thumbnail('post-thumbnails', array( 'class' => 'img-card-secondary' )) ?>
             </div>
             <div class="col-12 col-sm-12 col-md-12 col-lg-8 col-xl-8 d-flex flex-column justify-content-center">
                 <h4> <?php the_title() ?> </h4>
@@ -25,6 +22,10 @@
         </div>
         <hr>
     <?php endwhile; endif; ?>
+
+    <div class="card-body">
+        <?php get_template_part( 'template-parts/content', 'paginacion') ?>
+    </div>
 </div>
 
 <?php get_footer()?>
--- category-5.php
+++ category-5.php
@@ -5,10 +5,7 @@
     <h2>Videos</h2>
     <hr>
     <?php 
-        $posts = new WP_Query(array(
-        'cat' => '5',
-        ));
-        if ($posts->have_posts() ) : while ( $posts->have_posts() ) : $posts->the_post(); ?>
+        if (have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <div class="row pt-1">
             <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
                 <div class="embed-responsive embed-responsive-16by9">
@@ -22,6 +19,9 @@
         </div>
         <hr>
     <?php endwhile; endif; ?>
+    <div class="card-body">
+        <?php get_template_part( 'template-parts/content', 'paginacion') ?>
+    </div>
 </div>
 
 <?php get_footer()?>
