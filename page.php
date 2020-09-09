9b8ee6c05cc35d995598393303c9152644523922
David Jovellar <davidjovellar93@gmail.com>
2020-09-09 19:50:46 +0200

    Implemented Contact page

--- page.php
+++ page.php
@@ -2,9 +2,203 @@
 <?php wp_head() ?>
 <?php get_header() ?>
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <div class="container">
-            <h2> <?php the_title() ?></h2>
-            <?php the_content() ?>
+        <div class="container pt-5 font-text">
+            
+            <h2>Contacto</h2>
+            <hr>
+            <p>Si quieres ponerte en contacto con nosotros puedes escribirnos a <b>info@zonagigantes.com</b> o contactarnos a través de las redes sociales.</p>           
+            
+            <h2 class="pt-5">¿Quienes somos?</h2>
+            <hr>
+
+            <div class="row">
+                <div class="col-sm-5 col-md-4 col-lg-2 col-xl-2 pl-4 pl-sm-5 pl-md-5 pl-lg-4 pl-xl-4 pt-3 pt-sm-4 pt-md-0 pt-lg-0 pt-xl-0">
+                    <img src="<?php bloginfo('template_url');?>/images/profile-img/ruben.png" class="img-fluid" alt="Miembro Zona Gigantes - Ruben" width="120" height="120"></img>
+                </div>
+                <div class="col-12 col-sm-6 col-md-6 col-lg-10 col-xl-10 pt-3 pt-sm-0 pt-md-0 pt-lg-0 pt-xl-0 pl-4 pl-sm-0 pl-md-0 pl-lg-0 pl-xl-0">
+                    <div class="row pt-lg-4 pt-xl-4">
+                        <div class="col-lg-12 col-xl-12">
+                            <h5>
+                                Rubén F. Vargas 
+                                <a href="https://twitter.com/Rubenfvargas" class="pl-2" target="_blank"><img src="<?php bloginfo('template_url');?>/images/other-icons/icon-twitter.png" class="" width="25" height="20"></img></a>
+                            </h5>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            Barcelona, 1987
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            Co-creador y Director
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <p class="pt-3">Rubén es un enamorado de la cultura americana y en especial de NYC. Con la NFL europa y el Madden se enamoró de este apasionante deporte. La SB Perdida en 2001, fue el principio de este amor con los G-MEN.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-sm-5 col-md-4 col-lg-2 col-xl-2 pl-md-4 pl-lg- pl-xl-1">
+                    <img src="<?php bloginfo('template_url');?>/images/profile-img/jorge.png" class="img-fluid" alt="Miembro Zona Gigantes" width="170" height="170"></img>
+                </div>
+                <div class="col-sm-6 col-md-6 col-lg-10 col-xl-10 pt-3 pt-sm-4 pt-md-4 pt-lg-0 pt-xl-0 pl-4 pl-sm-0 pl-md-0 pl-lg-0 pl-xl-0">
+                    <div class="row pt-lg-4 pt-xl-5">
+                        <div class="col-12">
+                            <h5>
+                                Jorge Vico
+                                <a href="https://twitter.com/vico_lara" class="pl-2" target="_blank"><img src="<?php bloginfo('template_url');?>/images/other-icons/icon-twitter.png" class="" width="25" height="20"></img></a>
+                            </h5>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            Madrid, 1989
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            Co-creador y Director creativo
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <p class="pt-3">Jorge se aficionó a la NFL en el 2000 y a los New York Giants en 2004 de la mano de Kurt Warner.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="row pt-3">
+                <div class="col-sm-5 col-md-4 col-lg-2 col-xl-2 pt-sm-5 pt-md-4 pt-lg-1 pt-xl-1 pl-4 pl-sm-5 pl-md-5 pl-lg-4 pl-xl-4">
+                    <img src="<?php bloginfo('template_url');?>/images/profile-img/alex.png" class="img-fluid" alt="Miembro Zona Gigantes" width="130" height="130"></img>
+                </div>
+                <div class="col-sm-6 col-md-6 col-lg-10 col-xl-10 pt-3 pt-sm-0 pt-md-0 pt-lg-0 pt-xl-0 pl-4 pl-sm-0 pl-md-0 pl-lg-0 pl-xl-0">
+                    <div class="row pt-lg-2 pt-xl-4">
+                        <div class="col-12">
+                            <h5>
+                                Alejandro Caviedes
+                                <a href="https://twitter.com/alejandrocavie3" class="pl-2" target="_blank"><img src="<?php bloginfo('template_url');?>/images/other-icons/icon-twitter.png" class="" width="25" height="20"></img></a>
+                            </h5>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            Madrid, 1993
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            Editor gráfico
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <p class="pt-3">Alejandro lleva casado con la ciudad de Nueva York desde su niñez gracias al cine. Su primer contacto deportivo fueron los Knicks y el siguiente paso fueron los Giants, hasta convertirse en su equipo y deporte favorito.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-sm-5 col-md-4 col-lg-2 col-xl-2 pl-4 pl-sm-5 pl-md-5 pl-lg-4 pl-xl-5 pt-sm-5">
+                    <img src="<?php bloginfo('template_url');?>/images/profile-img/david.png" class="img-fluid" alt="Miembro Zona Gigantes" width="90" height="90"></img>
+                </div>
+                <div class="col-sm-6 col-md-6 col-lg-10 col-xl-10 pl-4 pl-sm-0 pl-md-0 pl-lg-0 pl-xl-0">
+                    <div class="row pt-4">
+                        <div class="col-12">
+                            <h5>
+                                David Quilez
+                                <a href="https://twitter.com/kails85" class="pl-2" target="_blank"><img src="<?php bloginfo('template_url');?>/images/other-icons/icon-twitter.png" class="" width="25" height="20"></img></a>
+                            </h5>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            Barcelona, 1985
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            Co-creador y Analista.
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <p class="pt-3">David empezó a enamorarse de los Giants desde pequeño viendo la pelicula "Little Giants", en 2007 visito Nueva York, se enamoro de la Gran Manzana y se compro la camiseta de un tal Eli Manning. Ese año los Giants ganaron la Superbowl a los imbatidos Patriots en una final para la historia.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+
+            <div class="row pt-2">
+                <div class="col-sm-5 col-md-4 col-lg-2 col-xl-2 pl-4 pl-sm-4 pl-md-5 pl-lg-4 pl-xl-4 pt-sm-4 pt-md-2">
+                    <img src="<?php bloginfo('template_url');?>/images/profile-img/cañi.png" class="img-fluid" alt="Miembro Zona Gigantes" width="140" height="140"></img>
+                </div>
+                <div class="col-sm-6 col-md-6 col-lg-10 col-xl-10 pt-3 pt-sm-0 pt-md-0 pt-lg-0 pt-xl-0 pl-4 pl-sm-0 pl-md-0 pl-lg-0 pl-xl-0">
+                    <div class="row pt-lg-0 pt-xl-4">
+                        <div class="col-12">
+                            <h5>
+                                Pablo Cañibano
+                                <a href="https://twitter.com/pablogigante7" class="pl-2" target="_blank"><img src="<?php bloginfo('template_url');?>/images/other-icons/icon-twitter.png" class="" width="25" height="20"></img></a>
+                            </h5>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            Oviedo, 1978
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            Escritor y Tertuliano.
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <p class="pt-3">Pablo sigue enamorado de la mejor defensa de la historia en 2012 cuando ganó al mejor equipo de la historia. Otra vez.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+
+            <div class="row pt-2">
+                <div class="col-12 col-sm-5 col-md-4 col-lg-2 col-xl-2 pl-4 pl-sm-5 pl-md-5 pl-lg-4 pl-xl-4 pt-3 pt-sm-0 pt-md-0 pt-lg-0 pt-xl-0">
+                    <img src="<?php bloginfo('template_url');?>/images/icon-teams/giants-icon.png" class="img-fluid" alt="Miembo Zona Gigantes" width="100" height="100">
+                </div>
+                <div class="col-12 col-sm-6 col-md-6 col-lg-10 col-xl-10 pt-3 pt-sm-0 pt-md-0 pt-lg-0 pt-xl-0 pl-4 pl-sm-0 pl-md-0 pl-lg-0 pl-xl-0">
+                    <div class="row pt-4">
+                        <div class="col-12">
+                            <h5>
+                                Ferran Àngel
+                                <a href="https://twitter.com/fangelb" class="pl-2" target="_blank"><img src="<?php bloginfo('template_url');?>/images/other-icons/icon-twitter.png" class="" width="25" height="20"></img></a>
+                            </h5>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            Mataró, 1985
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            Politólogo y Tertuliano.
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <p class="pt-3">Nueva York es la mejor ciudad del mundo -ahí, ahí con Barcelona- así que era imposible no convertirme en fiel aficionado del equipo que comandaba Eli Manning y dejaba a Brady sin su temporada perfecta. Y hasta hoy, cada día más fiel</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
         </div>
     <?php endwhile; endif; ?>
 <?php get_footer() ?>
