6cd7285d5c5f1418f94846c510095083fa179d3c
David Jovellar <davidjovellar93@gmail.com>
2021-07-29 20:53:48 +0200

    Player Stats - Define layout for Tables + Load image

--- category-estadisticas.php
+++ category-estadisticas.php
@@ -4,54 +4,53 @@
 
 <div class="container pt-5">
     <nav class="nav nav-pills flex-column flex-sm-row">
-        <a class="flex-sm-fill text-sm-center nav-link active" aria-controls="home" data-toggle="tab" role="tab" href="#general">General</a>
-        <a class="flex-sm-fill text-sm-center nav-link" aria-controls="profile" data-toggle="tab" role="tab" href="#pase">Pase</a>
-        <a class="flex-sm-fill text-sm-center nav-link" aria-controls="profile" data-toggle="tab" role="tab" href="#carrera">Carrera</a>
-        <a class="flex-sm-fill text-sm-center nav-link" aria-controls="profile2" data-toggle="tab" role="tab" href="#recepcion">Recepcion</a>
+        <a class="flex-sm-fill text-sm-center nav-link active" aria-controls="home" data-toggle="tab" role="tab" href="#pass">Pase</a>
+        <a class="flex-sm-fill text-sm-center nav-link" aria-controls="profile" data-toggle="tab" role="tab" href="#rush">Carrera</a>
+        <a class="flex-sm-fill text-sm-center nav-link" aria-controls="profile2" data-toggle="tab" role="tab" href="#reception">Recepcion</a>
         <a class="flex-sm-fill text-sm-center nav-link" aria-controls="profile2" data-toggle="tab" role="tab" href="#defensa">Defensa</a>
         <a class="flex-sm-fill text-sm-center nav-link" aria-controls="profile2" data-toggle="tab" role="tab" href="#equiposEspeciales">Equipos especiales</a>
     </nav>
     <hr>
-    <br>
     <div class="tab-content">
-        <div role="tabpanel" class="tab-pane fade show active" id="general">
+        <div role="tabpanel" class="tab-pane fade show active shadow" id="pass">
             <?php
                 global $wpdb;
-                $stats_passing = $wpdb->get_results("SELECT * FROM `stats_passing`");
+                $stats_passing = $wpdb->get_results("SELECT * FROM stats_passing ORDER BY cast(YDS as signed) DESC");
             ?>
-
             <div class="table-responsive">
                 <table class="table table-striped">
                     <thead>
                         <tr>
-                            <th scope="col" class="text-center">Foto</th>
-                            <th scope="col" class="text-center">Nombre</th>
-                            <th scope="col" class="text-center">Intentos</th>
-                            <th scope="col" class="text-center">Completados</th>
-                            <th scope="col" class="text-center">Yardas</th>
-                            <th scope="col" class="text-center">Completados %</th>
-                            <th scope="col" class="text-center">Yardas/Intento</th>
-                            <th scope="col" class="text-center">Touchdows</th>
-                            <th scope="col" class="text-center">Intercepciones</th>
-                            <th scope="col" class="text-center">Sacks</th>
+                            <th scope="col" class="text-center" style="width: 10%"></th>
+                            <th scope="col" class="text-center" style="width: 10%">JUGADOR</th>
+                            <th scope="col" class="text-center" style="width: 10%">ATT</th>
+                            <th scope="col" class="text-center" style="width: 10%">COMP</th>
+                            <th scope="col" class="text-center" style="width: 10%">YDS</th>
+                            <th scope="col" class="text-center" style="width: 10%">COMP%</th>
+                            <th scope="col" class="text-center" style="width: 10%">YDS/ATT</th>
+                            <th scope="col" class="text-center" style="width: 10%">TD</th>
+                            <th scope="col" class="text-center" style="width: 10%">INT</th>
+                            <th scope="col" class="text-center" style="width: 10%">SACKS</th>
                         </tr>
                     </thead>
                     <tbody>
                         <?php
-                            foreach($stats_passing as $stats_players) { ?>
+                            foreach($stats_passing as $stats_player) { ?>
                                 <tr>
-                                    <td class="text-center">
-                                        <img src="https://static.clubs.nfl.com/image/private/t_thumb_squared/f_auto/giants/v59trxgtfmixz7zt128a" class="img-fluid" width="50" height="50">
+                                    <td class="text-center align-middle" style="width: 10%; padding: 6px">
+                                        <img src="<?php bloginfo('template_url');?>/images/players/<?php echo $stats_player->Photo ?>"
+                                             onerror="this.src='<?php bloginfo('template_url');?>/images/no-image.png'"
+                                             class="img-fluid" width="80" height="80">
                                     </td>
-                                    <td class="text-center align-middle">Daniel Jones</td>
-                                    <td class="text-center align-middle">448</td>
-                                    <td class="text-center align-middle">280</td>
-                                    <td class="text-center align-middle">2943</td>
-                                    <td class="text-center align-middle">62.5%</td>
-                                    <td class="text-center align-middle">6.6</td>
-                                    <td class="text-center align-middle">11</td>
-                                    <td class="text-center align-middle">10</td>
-                                    <td class="text-center align-middle">45</td>
+                                    <td class="text-center align-middle font-weight-bold" style="width: 15%"><?php echo $stats_player->Player ?></td>
+                                    <td class="text-center align-middle" style="width: 10%"><?php echo $stats_player->ATT ?></td>
+                                    <td class="text-center align-middle" style="width: 10%"><?php echo $stats_player->COMP ?></td>
+                                    <td class="text-center align-middle" style="width: 10%"><?php echo $stats_player->YDS ?></td>
+                                    <td class="text-center align-middle" style="width: 10%"><?php echo $stats_player->{'COMP%'} ?></td>
+                                    <td class="text-center align-middle" style="width: 10%"><?php echo $stats_player->{'YDS/ATT'} ?></td>
+                                    <td class="text-center align-middle" style="width: 10%"><?php echo $stats_player->TD ?></td>
+                                    <td class="text-center align-middle" style="width: 10%"><?php echo $stats_player->INT ?></td>
+                                    <td class="text-center align-middle" style="width: 10%"><?php echo $stats_player->SCK ?></td>
                                 </tr>
                         <?php } ?>
                     </tbody>
@@ -59,13 +58,83 @@
             </div>
         </div>
 
-
-
-        <div role="tabpanel" class="tab-pane fade" id="pase">
-            <h1>PROVA 2</h1>
+        <div role="tabpanel" class="tab-pane fade shadow" id="rush">
+           <?php
+                global $wpdb;
+                $stats_passing = $wpdb->get_results("SELECT * FROM stats_rushing ORDER BY cast(YDS as signed) DESC");
+            ?>
+            <div class="table-responsive">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col" class="text-center" style="width: 10%"></th>
+                            <th scope="col" class="text-center" style="width: 15%">JUGADOR</th>
+                            <th scope="col" class="text-center" style="width: 15%">ATT</th>
+                            <th scope="col" class="text-center" style="width: 15%">YDS</th>
+                            <th scope="col" class="text-center" style="width: 15%">YDS/ATT</th>
+                            <th scope="col" class="text-center" style="width: 15%">LONG</th>
+                            <th scope="col" class="text-center" style="width: 15%">TD</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            foreach($stats_passing as $stats_player) { ?>
+                                <tr>
+                                    <td class="text-center align-middle" style="width: 10%; padding: 6px">
+                                        <img src="<?php bloginfo('template_url');?>/images/players/<?php echo $stats_player->Photo ?>"
+                                             onerror="this.src='<?php bloginfo('template_url');?>/images/no-image.png'"
+                                             class="img-fluid" width="80" height="80">
+                                    </td>
+                                    <td class="text-center align-middle font-weight-bold" style="width: 15%"><?php echo $stats_player->Player ?></td>
+                                    <td class="text-center align-middle " style="width: 15%"><?php echo $stats_player->ATT ?></td>
+                                    <td class="text-center align-middle " style="width: 15%"><?php echo $stats_player->YDS ?></td>
+                                    <td class="text-center align-middle " style="width: 15%"><?php echo $stats_player->{'YDS/ATT'} ?></td>
+                                    <td class="text-center align-middle " style="width: 15%"><?php echo $stats_player->LNG ?></td>
+                                    <td class="text-center align-middle " style="width: 15%"><?php echo $stats_player->TD ?></td>
+                                </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
-        <div role="tabpanel" class="tab-pane fade" id="carrera">
-            <h1>PROVA 3</h1>
+        <div role="tabpanel" class="tab-pane fade shadow" id="reception">
+           <?php
+                global $wpdb;
+                $stats_passing = $wpdb->get_results("SELECT * FROM stats_receiving ORDER BY cast(YDS as signed) DESC");
+            ?>
+            <div class="table-responsive">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col" class="text-center align-middle" style="width: 10%"></th>
+                            <th scope="col" class="text-center align-middle" style="width: 15%">JUGADOR</th>
+                            <th scope="col" class="text-center" style="width: 15%">REC</th>
+                            <th scope="col" class="text-center" style="width: 15%">YDS</th>
+                            <th scope="col" class="text-center" style="width: 15%">YDS/REC</th>
+                            <th scope="col" class="text-center" style="width: 15%">LONG</th>
+                            <th scope="col" class="text-center" style="width: 15%">TD</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            foreach($stats_passing as $stats_player) { ?>
+                                <tr>
+                                    <td class="text-center align-middle" style="width: 10%; padding: 6px">
+                                        <img src="<?php bloginfo('template_url');?>/images/players/<?php echo $stats_player->Photo ?>"
+                                             onerror="this.src='<?php bloginfo('template_url');?>/images/no-image.png'"
+                                             class="img-fluid" width="80" height="80">
+                                    </td>
+                                    <td class="text-center align-middle font-weight-bold" style="width: 15%"><?php echo $stats_player->Player ?></td>
+                                    <td class="text-center align-middle" style="width: 15%"><?php echo $stats_player->REC ?></td>
+                                    <td class="text-center align-middle" style="width: 15%"><?php echo $stats_player->YDS ?></td>
+                                    <td class="text-center align-middle" style="width: 15%"><?php echo $stats_player->{'YDS/REC'} ?></td>
+                                    <td class="text-center align-middle" style="width: 15%"><?php echo $stats_player->LNG ?></td>
+                                    <td class="text-center align-middle" style="width: 15%"><?php echo $stats_player->TD ?></td>
+                                </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 
