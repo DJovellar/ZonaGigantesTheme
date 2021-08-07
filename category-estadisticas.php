e737baaf5831c13ad45a7a4f842063805dd4b62c
David Jovellar <davidjovellar93@gmail.com>
2021-08-07 15:29:19 +0200

    Player Stats - Add staff

--- category-estadisticas.php
+++ category-estadisticas.php
@@ -2,7 +2,98 @@
 <?php wp_head() ?>
 <?php get_header() ?>
 
+<?php
+    global $wpdb;
+    $stats_passing = $wpdb->get_results("SELECT * FROM stats_passing ORDER BY cast(YDS as signed) DESC");
+    $stats_rushing = $wpdb->get_results("SELECT * FROM stats_rushing ORDER BY cast(YDS as signed) DESC");
+    $stats_receiving = $wpdb->get_results("SELECT * FROM stats_receiving ORDER BY cast(YDS as signed) DESC");
+    $stats_defense = $wpdb->get_results("SELECT
+                                            IF (t.Photo is not null, t.Photo, i.Photo) AS 'Photo',
+                                            IF (t.Player is not null, t.Player, i.Player) AS 'Player',
+                                            IF (t.TOTAL is not null, t.TOTAL, 0) AS 'TOTAL',
+                                            IF (t.SCK is not null, t.SCK, 0) AS 'SCK',
+                                            IF (t.F is not null, t.F, 0) AS 'F',
+                                            IF (i.INT is not null, i.INT, 0) AS 'INT',
+                                            IF (i.TDS is not null, i.TDS, 0) AS 'TDS'
+                                            FROM stats_defense_tackles t
+                                            LEFT JOIN stats_defense_ints i ON t.Photo = i.Photo ORDER BY cast(t.TOTAL as signed) DESC");
+
+?>
 <div class="container pt-5">
+    <div class="row no-gutters">
+        <div class="col-6 d-flex justify-content-center">
+            <div class="card" style="border: none;">
+                <div class="border-0 card-img-top">
+                    <img src="<?php bloginfo('template_url');?>/images/staff/Dave-Gettleman.png"
+                            onerror="this.src='<?php bloginfo('template_url');?>/images/no-image.png'"
+                            class="img-fluid" width="210" height="210">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title d-flex justify-content-center text-center">Dave Gettleman</h5>
+                    <p class="card-text d-flex justify-content-center text-center">General Manager</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-6 d-flex justify-content-center">
+            <div class="card" style="border: none;">
+                <div class="border-0 card-img-top">
+                    <img src="<?php bloginfo('template_url');?>/images/staff/Joe-Judge.png"
+                            onerror="this.src='<?php bloginfo('template_url');?>/images/no-image.png'"
+                            class="img-fluid" width="230" height="230">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title d-flex justify-content-center text-center">Joe Judge</h5>
+                    <p class="card-text d-flex justify-content-center text-center">Head Coach</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row pt-4 no-gutters">
+        <div class="col-4 d-flex justify-content-center">
+            <div class="card" style="border: none;">
+                <div class="border-0 card-img-top">
+                    <img src="<?php bloginfo('template_url');?>/images/staff/Jason-Garret.png"
+                            onerror="this.src='<?php bloginfo('template_url');?>/images/no-image.png'"
+                            class="img-fluid" width="210" height="210">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title d-flex justify-content-center text-center">Jason Garret</h5>
+                    <p class="card-text d-flex justify-content-center text-center">Coordinador Ofensivo</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-4 d-flex justify-content-center">
+            <div class="card" style="border: none;">
+                <div class="border-0 card-img-top">
+                    <img src="<?php bloginfo('template_url');?>/images/staff/Patrick-Graham.png"
+                            onerror="this.src='<?php bloginfo('template_url');?>/images/no-image.png'"
+                            class="img-fluid" width="210" height="210">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title d-flex justify-content-center text-center">Patrick Graham</h5>
+                    <p class="card-text d-flex justify-content-center text-center">Coordinador Defensivo</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-4 d-flex justify-content-center">
+            <div class="card" style="border: none;">
+                <div class="border-0 card-img-top">
+                    <img src="<?php bloginfo('template_url');?>/images/staff/Thomas-McGaughey.png"
+                            onerror="this.src='<?php bloginfo('template_url');?>/images/no-image.png'"
+                            class="img-fluid" width="210" height="210">
+                </div>
+                <div class="card-body">
+                    <h5 class="d-flex justify-content-center text-center">Thomas</h5>
+                    <h5 class="d-flex justify-content-center text-center" style="margin-top: 0;">McGaughey</h5>
+                    <span class="card-text d-flex justify-content-center text-center">Coordinador de </span>
+                    <span class="card-text d-flex justify-content-center text-center">Equipos Especiales </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container pt-4">
     <nav class="nav nav-pills flex-column flex-sm-row">
         <a class="flex-sm-fill text-sm-center nav-link active shadow" aria-controls="home" data-toggle="tab" role="tab" href="#pass">Pase</a>
         <a class="flex-sm-fill text-sm-center nav-link shadow" aria-controls="profile" data-toggle="tab" role="tab" href="#rush">Carrera</a>
@@ -12,10 +103,6 @@
     <br>
     <div class="tab-content">
         <div role="tabpanel" class="tab-pane fade show active shadow" id="pass">
-            <?php
-                global $wpdb;
-                $stats_passing = $wpdb->get_results("SELECT * FROM stats_passing ORDER BY cast(YDS as signed) DESC");
-            ?>
             <div class="table-responsive">
                 <table class="table table-striped" id="passTable">
                     <thead>
@@ -58,10 +145,6 @@
         </div>
 
         <div role="tabpanel" class="tab-pane fade shadow" id="rush">
-           <?php
-                global $wpdb;
-                $stats_rushing = $wpdb->get_results("SELECT * FROM stats_rushing ORDER BY cast(YDS as signed) DESC");
-            ?>
             <div class="table-responsive">
                 <table class="table table-striped" id="rushTable">
                     <thead>
@@ -97,10 +180,6 @@
             </div>
         </div>
         <div role="tabpanel" class="tab-pane fade shadow" id="reception">
-           <?php
-                global $wpdb;
-                $stats_receiving = $wpdb->get_results("SELECT * FROM stats_receiving ORDER BY cast(YDS as signed) DESC");
-            ?>
             <div class="table-responsive">
                 <table class="table table-striped" id="receivingTable">
                     <thead>
@@ -136,19 +215,6 @@
             </div>
         </div>
         <div role="tabpanel" class="tab-pane fade shadow" id="defense">
-           <?php
-                global $wpdb;
-                $stats_defense = $wpdb->get_results("SELECT
-                                                        IF (t.Photo is not null, t.Photo, i.Photo) AS 'Photo',
-                                                        IF (t.Player is not null, t.Player, i.Player) AS 'Player',
-                                                        IF (t.TOTAL is not null, t.TOTAL, 0) AS 'TOTAL',
-                                                        IF (t.SCK is not null, t.SCK, 0) AS 'SCK',
-                                                        IF (t.F is not null, t.F, 0) AS 'F',
-                                                        IF (i.INT is not null, i.INT, 0) AS 'INT',
-                                                        IF (i.TDS is not null, i.TDS, 0) AS 'TDS'
-                                                     FROM stats_defense_tackles t
-                                                     LEFT JOIN stats_defense_ints i ON t.Photo = i.Photo ORDER BY cast(t.TOTAL as signed) DESC");
-            ?>
             <div class="table-responsive">
                 <table class="table table-striped " id="defenseTable">
                     <thead>
@@ -157,7 +223,7 @@
                             <th scope="col" class="text-center align-middle" style="width: 15%">JUGADOR</th>
                             <th scope="col" class="text-center" style="width: 15%">TACKLES</th>
                             <th scope="col" class="text-center" style="width: 15%">SACKS</th>
-                            <th scope="col" class="text-center" style="width: 15%">FORCE FUMBLES</th>
+                            <th scope="col" class="text-center" style="width: 15%">F.FUMBLES</th>
                             <th scope="col" class="text-center" style="width: 15%">INT</th>
                             <th scope="col" class="text-center" style="width: 15%">TD</th>
                         </tr>
